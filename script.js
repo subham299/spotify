d01cb1e973acb21cb58d1672aa80bba95b7ba155
subham299 <143379903+subham299@users.noreply.github.com>
2023-08-29 13:44:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const song1 = document.getElementById("song1")
+
+function playpuse(){
+    if(song1.paused){
+        song1.play()
+    }else{
+        song1.paused()
+    }
+}
+
+const song2 = document.getElementById("song2")
+
+function playpuse(){
+    if(song2.paused){
+        song2.play()
+    }else{
+        song2.paused()
+    }
+}
